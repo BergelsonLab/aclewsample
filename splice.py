4e49bc84cc26e5f00576811b1e25dfd3a43eafeb
Andrei Amatuni <andrei.amatuni@gmail.com>
2017-09-26 15:18:25 -0400

    template gen works now

--- splice.py
+++ splice.py
@@ -19,15 +19,15 @@ def choose_onsets(f, n=5, t=2, start=30):
     try:
         length = metadata.streams[0].durationSeconds()
         length_min = int(length) / 60
-        min_range = range(start, length_min-t-3)
-        shuffled = shuffle(min_range)
+        minute_range = range(start, length_min-t)
+        shuffle(minute_range)
         selected = []
-        for x in shuffled:
+        for x in minute_range:
             if len(selected) >= n:
                 break
-            if not any(_overlap(x, y) for y in selected):
+            if not any(_overlap(x, y, t) for y in selected):
                 selected.append(x)
-        return [(x-2, x+3) for x in selected]
+        return [(x, x+2) for x in selected]
     except (IndexError):
         print  "{} was a problem file. skipped.".format(f)
 
@@ -38,18 +38,34 @@ def _splice(f, out_dir, timestamps):
         out_dir (str): directory to dump spliced output
         timestamps (list): list of onset/offset tuples
     """
+    spliced_outputs = []
     for i, x in enumerate(timestamps):
+        fname = os.path.join(out_dir, "{}_{}_{}-{}.wav".format(os.path.basename(f)[:-4], i+1, x[0], x[1]))
         command = ["ffmpeg",
-                "-ss", "{}".format(x[0]),
-                "-t", "{}".format(x[1]-x[0]),
+                "-ss", "{}".format(x[0]*60),
                 "-i", f,
-                "-acodec", "copy",
-                    os.path.join(out_dir, "{}_{}_{}-{}.mp3".format(f, i, x[0], x[1]))
-                ]
+                "-t", "{}".format(x[1] * 60 - x[0] * 60),
+                "-acodec", "copy", fname]
+        spliced_outputs.append(fname)
         sp.call(command)
+    concat_out = os.path.join(out_dir, "{}_concat_all.wav".format(os.path.basename(f)[:-4]))
+    # concat_cmd = [
+    #     "ffmpeg",
+    #     "-i", "\"concat:{}\"".format("|".join(spliced_outputs)),
+    #     "-c", "copy", concat_out
+    # ]
 
-def _overlap(x, y):
-    pass
+    concat_cmd = "ffmpeg -i \"concat:{}\" -c copy {}".format("|".join(spliced_outputs), concat_out, shell=True)
+    print concat_cmd
+    sp.call(concat_cmd)
+
+def _overlap(x, y, t):
+    if y < x < y+t:
+        return True
+    elif y-t < x < y:
+        return True
+    return False
+
 
 def splice(input_dir, output_dir):
     for root, dirs, files in os.walk(input_dir):
@@ -63,6 +79,5 @@ if __name__ == "__main__":
     input_dir = sys.argv[1]
     output_dir = sys.argv[2]
 
-
-    # choose_onsets()
+    splice(input_dir, output_dir)
 
--- templgen.py
+++ templgen.py
@@ -1,8 +1,29 @@
 import sys
+import os
 
-def generate_template():
-    pass
+import splice
+import pympi
+
+
+def generate_templates(input_dir, output_dir):
+    for root, dirs, files in os.walk(input_dir):
+        for file in files:
+            if file.endswith(".mp3") or file.endswith(".wav"):
+                timestamps = splice.choose_onsets(os.path.join(root, file))
+                timestamps = [(x*60000,y*60000) for x, y in timestamps]
+                print timestamps
+                eaf = pympi.Eaf()
+                eaf.add_tier("code")
+                eaf.add_tier("context")
+                eaf.remove_tier("default")
+                for ts in timestamps:
+                    eaf.add_annotation("code", ts[0], ts[1])
+                    eaf.add_annotation("context", ts[0]-120000, ts[1]+60000)
+                eaf.to_file(os.path.join(output_dir, "{}.eaf".format(file[:-4])))
 
 if __name__ == "__main__":
 
     input_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+
+    generate_templates(input_dir, output_dir)
