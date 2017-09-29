f8cc4c6a2a4ee8a9fe8c8f1ab55fdc242dda2abc
Andrei Amatuni <andrei.amatuni@gmail.com>
2017-09-29 14:06:26 -0400

    implement std_mat_ed weighting

--- sample.py
+++ sample.py
@@ -6,12 +6,50 @@ corpora_file = "data/ACLEW_list_of_corpora.csv"
 def unique_n_children(df, n, exclude_ids=[]):
     selected = pd.DataFrame(columns = df.columns.values)
     while selected.shape[0] < n:
-        picked = df.sample(n=1)
+        picked = _uniform_category_sample(df, n=1, cat='std_mat_ed')
         picked_id = picked.iloc[0]['child_level_id']
-        if picked_id not in selected['child_level_id'].values.tolist() + exclude_ids:
+
+        # if picked child not already picked from previous session
+        # (when picking the 2x 9mo < age < 14mo), passed in as exclude_ids,
+        # then...
+        if picked_id not in exclude_ids:
+                # add to selected
                 selected = selected.append(picked)
-                df = df.drop(picked.index)
+                # remove records from selection pool with the same child_level_id,
+                # child has been selected once, out of the pool for future selections
+                df = df.drop(df.index[df['child_level_id'] == picked_id])
     return selected
+
+def _uniform_category_sample(df, n, cat):
+    """
+    Normalize P(x_i), where x_i is the i'th member of category x
+    so that the distribution across categories is uniform. Solution is
+    to set
+
+    P(x_i) = (1/K) / |x|
+
+    where K is the # of categories and |x| is the frequency of
+    elements of category x.
+
+    :param df:
+    :return:
+    """
+
+    df['weight'] = 0
+    categories = df[cat].unique()
+    K = categories.shape[0]
+
+    # calculate weights
+    for name, group, in df.groupby(cat):
+        n_x = group.shape[0] # |x| (number of records with this category)
+        p_i = (1/float(K))/n_x
+        df.loc[df[cat] == name, 'weight'] = p_i
+
+    # pick n samples, given the weights
+    picked = df.sample(n, weights='weight')
+
+    return picked
+
 
 def _sample(corpus):
     '''
@@ -37,6 +75,8 @@ def sample(aclew_corpora_file, output_csv=""):
     selected = pd.DataFrame(columns = corpora.columns.values)
     # group by corpus and sample
     for name, corpus in corpora.groupby('corpus'):
+        if name == "Seedlings":
+            print
         the_2, the_8 = _sample(corpus)
         selected = selected.append([the_2, the_8])
     if output_csv:
